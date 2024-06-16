e98fd6470bff7ec9defd0c4d91be61d2b8a6d56f
mfnsvrtm <74641121+mfnsvrtm@users.noreply.github.com>
2024-06-16 17:12:12 +0400

    move some of the code into the AppOverlay component

--- src/App.tsx
+++ src/App.tsx
@@ -2,79 +2,11 @@ import { AppBar, Box, Container, Toolbar, Typography } from '@mui/material';
 import Board from './components/board/Board';
 import CursorOverrideProvider from './providers/CursorOverrideProvider';
 import { BoardContextProvider } from './providers/BoardContextProvider';
-import { BoardSave, useBoard } from './hooks/useBoard';
-import useLocalStorage from './hooks/useLocalStorage';
-import InfoCard from './components/InfoCard';
-import SideMenu from './components/SideMenu';
-import LoadDialog from './components/dialogs/LoadDialog';
-import { settingsDialogContent } from './components/dialogs/SettingsDialog';
-import { useState } from 'react';
-import { nanoid } from 'nanoid';
-import { clearDialogContent } from './components/dialogs/ClearDialog';
-import useDialog from './hooks/useDialog';
-import { saveDialogContent } from './components/dialogs/SaveDialog';
-
-interface KanbanLikeStorage {
-  saves: { id: string, name: string, data: BoardSave; }[];
-  showInfo: boolean;
-}
+import { useBoard } from './hooks/useBoard';
+import AppOverlay from './components/AppOverlay';
 
 const App = (): React.ReactNode => {
-  const [showLoadDialog, setShowLoadDialog] = useState(false);
-  const { storedValue, setValue } = useLocalStorage<KanbanLikeStorage>('kanbanLikeStore', { saves: [], showInfo: true });
   const context = useBoard();
-
-  const saveData = storedValue.saves.map(save => ({ id: save.id, name: save.name }));
-
-  const openSaveDialog = useDialog({
-    content: saveDialogContent,
-    onAccept: (data: FormData) => {
-      setValue({
-        ...storedValue,
-        saves: [...storedValue.saves, { id: nanoid(), name: data.get('name') as string, data: context.save() }]
-      });
-    },
-    sx: { width: 500 }
-  });
-
-  const handleLoadDialogSuccess = (id: string) => {
-    setShowLoadDialog(false);
-    context.load(storedValue.saves.find(save => save.id === id)!.data);
-  };
-
-  const handleLoadDialogClose = () => {
-    setShowLoadDialog(false);
-  };
-
-  const openClearDialog = useDialog({
-    content: clearDialogContent,
-    onAccept: () => context.clear()
-  });
-
-  const openSettingsDialog = useDialog({
-    content: settingsDialogContent,
-    onAccept: (data: FormData) => {
-      setValue({
-        ...storedValue,
-        showInfo: Boolean(data.get('showInfo')),
-      });
-    },
-    props: { showInfo: storedValue.showInfo }
-  });
-
-  const handleDontShowThisAgain = () => {
-    setValue({
-      ...storedValue,
-      showInfo: false,
-    });
-  };
-
-  const handleDeleteSave = (id: string) => {
-    setValue({
-      ...storedValue,
-      saves: storedValue.saves.filter(save => save.id !== id)
-    });
-  };
 
   return (
     <Box height={'100vh'}>
@@ -88,11 +20,9 @@ const App = (): React.ReactNode => {
         </Container>
       </AppBar>
       <Box pt={8} height='100%' minWidth='100vw' display='inline-grid' gridTemplateColumns='minmax(300px, 1fr) auto minmax(300px, 1fr)'>
-        {showLoadDialog ? <LoadDialog data={saveData} onClose={handleLoadDialogClose} onLoad={handleLoadDialogSuccess} onDelete={handleDeleteSave} /> : null}
-        <SideMenu onSave={openSaveDialog} onLoad={() => setShowLoadDialog(true)} onClear={openClearDialog} onSettings={openSettingsDialog} />
-        {storedValue.showInfo ? <InfoCard onDontShowAgain={handleDontShowThisAgain} /> : null}
         <CursorOverrideProvider>
           <BoardContextProvider context={context}>
+            <AppOverlay />
             <Board sx={{ gridColumn: 2 }} />
           </BoardContextProvider>
         </CursorOverrideProvider>
---
+++ src/components/AppOverlay.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import useDialog from '../hooks/useDialog';
+import { useBoardContext } from '../providers/BoardContextProvider';
+import { nanoid } from 'nanoid';
+import InfoCard from './InfoCard';
+import SideMenu from './SideMenu';
+import LoadDialog from './dialogs/LoadDialog';
+import { settingsDialogContent } from './dialogs/SettingsDialog';
+import { clearDialogContent } from './dialogs/ClearDialog';
+import { saveDialogContent } from './dialogs/SaveDialog';
+import { BoardSave } from '../hooks/useBoard';
+
+export interface KanbanLikeStorage {
+  saves: { id: string, name: string, data: BoardSave; }[];
+  showInfo: boolean;
+}
+
+const AppOverlay = (): React.ReactNode => {
+  const { storedValue, setValue } = useLocalStorage<KanbanLikeStorage>('kanbanLikeStore', { saves: [], showInfo: true });
+  const [showLoadDialog, setShowLoadDialog] = useState(false);
+  const saveData = storedValue.saves.map(save => ({ id: save.id, name: save.name }));
+  const context = useBoardContext();
+
+  const openSaveDialog = useDialog({
+    content: saveDialogContent,
+    onAccept: (data: FormData) => {
+      setValue({
+        ...storedValue,
+        saves: [...storedValue.saves, { id: nanoid(), name: data.get('name') as string, data: context.save() }]
+      });
+    },
+    sx: { width: 500 }
+  });
+
+  const openClearDialog = useDialog({
+    content: clearDialogContent,
+    onAccept: () => context.clear()
+  });
+
+  const openSettingsDialog = useDialog({
+    content: settingsDialogContent,
+    onAccept: (data: FormData) => {
+      setValue({
+        ...storedValue,
+        showInfo: Boolean(data.get('showInfo')),
+      });
+    },
+    props: { showInfo: storedValue.showInfo }
+  });
+
+  const handleLoadDialogSuccess = (id: string) => {
+    setShowLoadDialog(false);
+    context.load(storedValue.saves.find(save => save.id === id)!.data);
+  };
+
+  const handleLoadDialogClose = () => {
+    setShowLoadDialog(false);
+  };
+
+  const handleDeleteSave = (id: string) => {
+    setValue({
+      ...storedValue,
+      saves: storedValue.saves.filter(save => save.id !== id)
+    });
+  };
+
+  const handleDontShowThisAgain = () => {
+    setValue({
+      ...storedValue,
+      showInfo: false,
+    });
+  };
+
+  return (
+    <>
+      <SideMenu onSave={openSaveDialog} onLoad={() => setShowLoadDialog(true)} onClear={openClearDialog} onSettings={openSettingsDialog} />
+      {showLoadDialog ? <LoadDialog data={saveData} onClose={handleLoadDialogClose} onLoad={handleLoadDialogSuccess} onDelete={handleDeleteSave} /> : null}
+      {storedValue.showInfo ? <InfoCard onDontShowAgain={handleDontShowThisAgain} /> : null}
+    </>
+  );
+};
+
+export default AppOverlay;
